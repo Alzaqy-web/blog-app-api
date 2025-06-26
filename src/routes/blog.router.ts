f83ef3665113c2abf37db2b88a2ad6e2eec13996
AL ZAQY <alzaqy19@gmail.com>
2025-06-26 20:46:55 +0700

    feat: get detail blog

--- src/routes/blog.router.ts
+++ src/routes/blog.router.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import {
   CreateBlogController,
+  getBlogBySlugController,
   getBlogsController,
 } from "../controller/blog.controller";
 
 const router = express.Router();
 
 router.get("/", getBlogsController);
+router.get("/:slug", getBlogBySlugController);
 router.post("/", CreateBlogController);
 
 export default router;
